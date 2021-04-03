ec3343427e2c6a8159d0833927b52696a016c39d
Charlie Page <charles.david.page@gmail.com>
2021-04-03 12:25:41 -0500

    Initial Commit

---
+++ GoalTogether/GoalTogetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalTogetherApp.swift
+//  GoalTogether
+//
+//  Created by Charlie Page on 4/3/21.
+//
+
+import SwiftUI
+
+@main
+struct GoalTogetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
